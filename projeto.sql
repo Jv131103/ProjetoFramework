bcd2b2c652933f716bfeba635567b6f7fad80dca
Jv131103 <jjv1792@gmail.com>
2023-09-22 16:27:19 -0300

    Banco Atualizado

--- projeto.sql
+++ projeto.sql
@@ -7,7 +7,10 @@ SELECT @@hostname;
 
 -- Remover tabelas, mas apenas para casos extremos
 DROP TABLE Usuario;
-DROP TABLE TipoUsuario; -- Pode adicionar outra no lugar!
+DROP TABLE TipoUsuario;
+
+DROP TABLE Labs;
+DROP TABLE Uso_Lab;
 
 CREATE TABLE TipoUsuario(
 	id INT NOT NULL AUTO_INCREMENT,
@@ -27,19 +30,46 @@ CREATE TABLE Usuario(
     atividade VARCHAR(1) NOT NULL,
     CONSTRAINT pk_id_Usuario PRIMARY KEY (id),
     CONSTRAINT uk_cpf_Usuario UNIQUE (cpf),
+    CONSTRAINT uk_email_Usuario UNIQUE (email),
 	CONSTRAINT fk_id_TipoUsuario_Usuario FOREIGN KEY (id_tipo) REFERENCES TipoUsuario (id)
 );
 
--- Testes de inserções
+CREATE TABLE Uso_Lab(
+	id INT NOT NULL AUTO_INCREMENT,
+    resultado VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_id_Uso_Lab PRIMARY KEY (id)
+) AUTO_INCREMENT = 0;
+
+
+CREATE TABLE Labs (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    tipo_status INT NOT NULL,
+    disponibilidade INT NOT NULL, -- Para disponível 1 e para indisponível 2
+    CONSTRAINT pk_id_Labs PRIMARY KEY (id),
+    CONSTRAINT fk_registro_Labs_Uso_Lab FOREIGN KEY (tipo_status) REFERENCES Uso_Lab (id)
+);
+
+
+-- Testes de inserções USUÁRIOS
 INSERT INTO TipoUsuario(nome_tipo)
-VALUES('professor'), ('alunos');
+VALUES('professor'), ('alunos'), ('admin');
 
 INSERT INTO Usuario(nome, email, senha, cpf, telefone, id_tipo, atividade)
-VALUES('NomeProfessor', 'email@professor', 'SenhaProfessor', 'cpfProfessor',
-'telefoneProfessor', 1, '1'), ('NomeAluno', 'email@aluno', 'SenhaAluno', 
-'cpfAluno', 'telefoneAluno', 2, '0');
+VALUES('Joao', 'joao@email.com', 'jvsenha', '78509099332', '9912345678', 3, '1');
+
+-- Teste inserção Labs
+INSERT INTO Uso_Lab(resultado)
+VALUES('Ativado'), ('Desativado');
+
+INSERT INTO Labs(nome, tipo_status, disponibilidade)
+VALUES("salax", 0, 0), ("salay", 1, 1);
 
 -- Verificar Respostas
-
 SELECT * FROM TipoUsuario;
 SELECT * FROM Usuario;
+SELECT * FROM Uso_Lab;
+SELECT * FROM Labs;
+
+-- SELECT id FROM Usuario WHERE email='Bia@Bia' AND senha='123';
+
